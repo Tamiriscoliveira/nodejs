0716b355ec86bad67082c6d3f2eca205ba9e66c2
Tamiris Coutinho <tami_coliveira@hotmail.com>
2021-12-13 22:49:05 -0300

    exemplos aula 1 de fundamentos

---
+++ type-boolean.js
@@ -0,0 +1,19 @@
+//tipo boolean
+// true => verdadeiro
+// false => falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+//comparação dos valores
+// = um igual atribui valores
+// === tres iguais são comparações
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "TeamLC";
+const texto2 = "Alura";
+console.log(texto1 === texto2)
+
+
+
